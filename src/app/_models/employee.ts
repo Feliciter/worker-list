68fcce79cbc2b38e3a5254aa0e6ff12c36d6e2f7
Feliciter <padakluev@gmx.com>
2020-08-29 01:20:18 +0300

    add: first conf

---
+++ src/app/_models/employee.ts
@@ -0,0 +1,12 @@
+export class Employee {
+    id: string;
+    firstName: string;
+    lastName: string;
+    sex:string;
+    phone:number;
+    dob:Date;
+    position:string;
+    salary:number;
+    addinfo:string;
+    dtcreate:Date;    
+}
